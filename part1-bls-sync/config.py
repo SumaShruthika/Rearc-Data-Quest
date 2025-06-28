3eecdc883e46d72ad2d91419d3c985a096430240
SumaShruthika <sumamaheshsns@gmail.com>
2025-06-28 09:55:42 -0500

    modified User Agent

--- part1-bls-sync/config.py
+++ part1-bls-sync/config.py
@@ -2,5 +2,5 @@ BLS_URL = "https://download.bls.gov/pub/time.series/pr/"
 BUCKET_NAME = "rearc-data-quest-ssm"
 S3_PREFIX = "bls/pr/"
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36"
+    'User-Agent': 'RearcDataQuest (contact: sumashruthikamaheshkumar@gmail.com)'
 }
